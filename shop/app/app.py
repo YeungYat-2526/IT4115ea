a96fb3ec95af804251a52aeb6800d0a659f6ca70
Wilson-ycf <240106857@stu.vtc.edu.hk>
2026-04-27 05:48:48 +0000

    Change development server port to 5000

--- shop/app/app.py
+++ shop/app/app.py
@@ -486,4 +486,4 @@ if __name__ == '__main__':
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
